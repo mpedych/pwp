5baf4e01be1009c958dca44b3dc1a2eba3c77b4c
mpedych <maksym.pedych@gmail.com>
2024-12-28 23:17:54 +0200

    Initial commit. spring initializr version

---
+++ src/main/java/com/pedych/maksym/pwp/PwpApplication.java
@@ -0,0 +1,13 @@
+package com.pedych.maksym.pwp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedych/maksym/pwp/PwpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedych.maksym.pwp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
