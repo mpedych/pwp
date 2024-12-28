12e44e4dbd8bcb80c2a9e5ea80b58665c6d17aa4
mpedych <maksym.pedych@gmail.com>
2024-12-29 00:42:49 +0200

    Application class rename

---
+++ src/test/java/com/pedych/maksym/pwp/ApplicationTests.java
@@ -0,0 +1,37 @@
+package com.pedych.maksym.pwp;
+
+import com.pedych.maksym.pwp.controller.BlogController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(controllers = BlogController.class)
+class ApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void homePage() throws Exception {
+        // N.B. jsoup can be useful for asserting HTML content
+        mockMvc.perform(get("/index.html"))
+                .andExpect(content().string(containsString("Get your greeting")));
+    }
+
+    @Test
+    public void greeting() throws Exception {
+        mockMvc.perform(get("/greeting"))
+                .andExpect(content().string(containsString("Hello, World!")));
+    }
+
+    @Test
+    public void greetingWithUser() throws Exception {
+        mockMvc.perform(get("/greeting").param("name", "Greg"))
+                .andExpect(content().string(containsString("Hello, Greg!")));
+    }
+}
